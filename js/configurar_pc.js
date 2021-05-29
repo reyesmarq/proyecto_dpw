e65e8f5e6c179993248c33d97bf851f93075bfff
Mau Reyes <reyes.marq@gmail.com>
2021-05-29 13:00:54 -0600

    Agregar configurar pc

---
+++ js/configurar_pc.js
@@ -0,0 +1,11 @@
+(() => {
+  // construyendo la logica una vez el dom haya cargado completamente
+  document.addEventListener('DOMContentLoaded', (e) => {
+    let total = 0;
+    let totalElement = document.getElementById('total');
+
+    totalElement.textContent = "8080"
+
+    alert(nombre);
+  });
+})();
