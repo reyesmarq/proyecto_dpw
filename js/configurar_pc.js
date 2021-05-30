94df3d2f3a7028dfecb9291d61848af243eeeb61
Mau Reyes <reyes.marq@gmail.com>
2021-05-29 22:59:03 -0600

    refactor: event listeners

--- js/configurar_pc.js
+++ js/configurar_pc.js
@@ -15,129 +15,36 @@
 
   // construyendo la logica una vez el dom haya cargado completamente
   document.addEventListener('DOMContentLoaded', (e) => {
-    let MarcaProcesador = document.getElementById(KEYS.marcaProcesador);
-    let Procesador = document.getElementById(KEYS.procesador);
-    let Disipador = document.getElementById(KEYS.disipador);
-    let TarjetaMadre = document.getElementById(KEYS.tarjetaMadre);
-    let MemoriaRam = document.getElementById(KEYS.memoriaRam);
-    let MemoriaRamAdicional = document.getElementById(KEYS.memoriaRamAdicional);
-    let Almacenamiento = document.getElementById(KEYS.almacenamiento);
-    let AlmacenamientoAdicional = document.getElementById(
-      KEYS.almacenamientoAdicional
-    );
-    let TarjetaGrafica = document.getElementById(KEYS.tarjetaGrafica);
-    let Gabinete = document.getElementById(KEYS.gabinete);
-    let Fuente = document.getElementById(KEYS.fuente);
+    let ConfigurarPC = document.getElementById('configurarPc');
 
     // ! Asignarlo a falso para la entrega.
     let resetLocalStorage = true;
     if (resetLocalStorage) localStorage.clear();
 
-    // Paso 1
-    MarcaProcesador.addEventListener('click', (e) => {
-      let value = getDataset(e, KEYS.marcaProcesador);
-      localStorage.setItem(KEYS.marcaProcesador, value);
-      // TODO: Open the next accordion item
-    });
+    // Evento click
+    ConfigurarPC.addEventListener('click', (e) => {
+      let isTargetInRange = false;
+      let value;
+      let target;
 
-    // Paso 2
-    Procesador.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.procesador);
+      for (let key in KEYS) {
+        if (e.target.dataset[KEYS[key]]) {
+          isTargetInRange = true;
+          value = getDataset(e, KEYS[key]);
+          target = key;
+        }
+      }
 
-      if (price == null) return;
+      if (!isTargetInRange) return;
+      if (value == null) return;
 
-      updateTotal(price, KEYS.procesador);
-      // TODO: Open the next accordion item
-    });
+      // Si es marca procesador, no hay necesidad de actualizar el total
+      if (target == KEYS.marcaProcesador) {
+        localStorage.setItem(target, value);
+        return;
+      }
 
-    // Paso 3
-    Disipador.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.disipador);
-
-      if (price == null) return;
-
-      updateTotal(price, KEYS.disipador);
-      // TODO: Open the next accordion Item
-    });
-
-    // Paso 4
-    TarjetaMadre.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.tarjetaMadre);
-
-      if (price == null) return;
-
-      updateTotal(price, KEYS.tarjetaMadre);
-      // TODO: Open the next accordion Item
-    });
-
-    // Paso 5
-    MemoriaRam.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.memoriaRam);
-
-      if (price == null) return;
-
-      updateTotal(price, KEYS.memoriaRam);
-      // TODO: Open the next accordion Item
-    });
-
-    // Paso 6
-    MemoriaRamAdicional.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.memoriaRamAdicional);
-
-      if (price == null) return;
-
-      updateTotal(price, KEYS.memoriaRamAdicional);
-      // TODO: Open the next accordion Item
-    });
-
-    // Paso 7
-    Almacenamiento.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.almacenamiento);
-
-      if (price == null) return;
-
-      updateTotal(price, KEYS.almacenamiento);
-      // TODO: Open the next accordion item
-    });
-
-    // Paso 8
-    AlmacenamientoAdicional.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.almacenamientoAdicional);
-
-      if (price == null) return;
-
-      updateTotal(price, KEYS.almacenamientoAdicional);
-      // TODO: Open the next accordion item
-    });
-
-    // Paso 9
-    TarjetaGrafica.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.tarjetaGrafica);
-
-      if (price === null) return;
-
-      updateTotal(price, KEYS.tarjetaGrafica);
-      // TODO: Open the next accordion item
-    });
-
-    // Paso 10
-    Gabinete.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.gabinete);
-
-      if (price === null) return;
-
-      updateTotal(price, KEYS.gabinete);
-      // TODO: Open the next accordion item
-    });
-
-    // Paso 11
-    Fuente.addEventListener('click', (e) => {
-      let price = getDataset(e, KEYS.fuente);
-
-      if (price === null) return;
-
-      updateTotal(price, KEYS.fuente);
-      // TODO: Open the next accordion item
+      updateTotal(value, target);
     });
   });
 
